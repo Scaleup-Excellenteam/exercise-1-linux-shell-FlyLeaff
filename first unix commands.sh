ce3558220f64b41aa61d887ab72347ceac6b0e49
FlyLeaff <kohav24@gmail.com>
2024-05-12 19:32:56 +0300

    first question commited

---
+++ first unix commands.sh
@@ -0,0 +1,21 @@
+1:
+touch q1
+echo kohav buskila > q1
+printf "kohav buskila" >> q1
+2:
+mkdir new
+3-4:
+cp q1 new/kohav_buskila
+6:
+cp new/kohav_buskila new/newFile
+7:
+cd new
+ls
+8:
+cd $HOME
+or
+cd ~
+or
+cd.. cd.. cd.. ....
+rm -r new
+
