2b339753d1860cc9d66802fc29ad7a4eebb59c56
FlyLeaff <kohav24@gmail.com>
2024-05-13 16:25:46 +0300

    added 1 and 2

---
+++ Use the man.sh
@@ -0,0 +1,18 @@
+1:
+cd etc
+ls -lhS
+2:
+ls -lhSr
+smallest is a subuid- file at near 0
+largest is mime.types at 71k
+3: 
+mkdir -p grandpa/father/.son
+4:
+bc 
+5:
+bc -q
+6:
+cal
+6.a
+cal -y 2024
+
